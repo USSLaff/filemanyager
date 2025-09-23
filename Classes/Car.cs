acab5922b030eed82f15bdecb7349e5311896d9c
Laff <gerreger0170@gmail.com>
2025-09-23 11:30:14 +0200

    yippiee

---
+++ Classes/Car.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file_manager.Classes
+{
+    internal class Car
+    {
+        private string licensePlate;
+        private string brand;
+        private string name;
+        private string color;
+        private double price;
+        private int horsePower;
+        private double[] consumption = new double[3];
+        private bool isEconomy;
+
+      
+
+        public string LicensePlate
+        {
+            get { return licensePlate; }
+            set
+            {
+                if (IsValidLicensePlate(value))
+                {
+                    licensePlate = value.ToUpper();
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid license plate format. Valid formats: ABC-123 or AA-AA-123");
+                }
+            }
+        }
+        public int HorsePower{
+            get { return horsePower; }
+            set {
+                if (value < 0)
+                {
+                    throw new Exception("Horse power can't be negative or is it a JUNK?");
+                }
+                    else { 
+                        horsePower = value;
+                    }
+                }
+            
+            }
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Price cannot be negative");
+                }
+                price = value;
+            }
+        }
+
+        public double[] Consumption
+        {
+            get { return consumption; }
+            set
+            {
+                if (value.Length != 3)
+                {
+                    throw new ArgumentException("Consumption array must contain exactly 3 values");
+                }
+                consumption = value;
+                UpdateEconomyStatus();
+            }
+        }
+
+        public bool IsEconomy
+        {
+            get { return isEconomy; }
+        }
+
+        public static bool IsValidLicensePlate(string plate)
+        {
+            if (string.IsNullOrEmpty(plate))
+                return false;
+
+            plate = plate.ToUpper();
+            if (plate.Length == 7 && plate[3] == '-')
+            {
+                string firstPart = plate.Substring(0, 3);
+                string secondPart = plate.Substring(4, 3);
+
+                return secondPart.All(char.IsDigit) &&
+                       firstPart.All(c => c >= 'A' && c <= 'Z');
+            }
+
+            if (plate.Length == 9 && plate[2] == '-' && plate[5] == '-')
+            {
+                string part1 = plate.Substring(0, 2);
+                string part2 = plate.Substring(3, 2);
+                string part3 = plate.Substring(6, 3);
+
+                return part1.All(c => c >= 'A' && c <= 'Z') &&
+                       part2.All(c => c >= 'A' && c <= 'Z') &&
+                       part3.All(char.IsDigit);
+            }
+
+            return false;
+        }
+
+        
+        public void SetConsumption(double city, double mixed, double highway)
+        {
+            if (city < 0 || mixed < 0 || highway < 0)
+            {
+                throw new ArgumentException("Consumption values cannot be negative");
+            }
+
+            consumption[0] = city;
+            consumption[1] = mixed;
+            consumption[2] = highway;
+            UpdateEconomyStatus();
+        }
+
+        private void UpdateEconomyStatus()
+        {
+            isEconomy = consumption[1] <= 10; 
+        }
+
+        public double GetCityConsumption() => consumption[0];
+        public double GetMixedConsumption() => consumption[1];
+        public double GetHighwayConsumption() => consumption[2];
+
+
+        public override string ToString()
+        {
+            return $"\nLicense plate: {licensePlate}\nBrand: {brand}\nName: {name}\nColor: {Color}\nHorse Power: {horsePower}\nPrice: {price} HUF\nConsumption: \n\tCity: {consumption[0]} L/100Km\n\tMixed: {consumption[1]} L/100Km\n\tHighway: {consumption[2]} L/100Km\nEconomy: {(isEconomy ? "Cheap":"Expensive")}";
+        }
+
+        public Car()
+        {
+        }
+        public Car(string licensePlate, string brand, string name, int horsepower, string color, double price, double cityConsumption, double mixedConsumption, double highwayConsumption)
+        {
+            LicensePlate = licensePlate;
+            Brand = brand;
+            Name = name;
+            HorsePower = horsepower;
+            Color = color;
+            Price = price;
+            SetConsumption(cityConsumption, mixedConsumption, highwayConsumption);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using file_manager.Classes;
+using System.Runtime.InteropServices;
+using System.Runtime.ExceptionServices;
+
+namespace file_manager
+{
+    internal class Program
+    {
+        const string fileName = "data.bin";
+        public static void WriteToFile()
+        {
+            using (var stream = File.Open(fileName, FileMode.Create))
+            {
+                using (var writer = new BinaryWriter(stream, Encoding.UTF8, false))
+                {
+                    writer.Write(1.250F);
+                    writer.Write(@"c:\Temp");
+                    writer.Write(10);
+                    writer.Write(true);
+                }
+
+
+            }
+        }
+        public static void DisplayValues()
+        {
+            float aspectRatio;
+            string tempDirectory;
+            int autoSaveTime;
+            bool showStatusBar;
+
+            if (File.Exists(fileName))
+            {
+                using (var stream = File.Open(fileName, FileMode.Open))
+                {
+                    using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+                    {
+                        aspectRatio = reader.ReadSingle();
+                        tempDirectory = reader.ReadString();
+                        autoSaveTime = reader.ReadInt32();
+                        showStatusBar = reader.ReadBoolean();
+                    }
+                }
+
+                Console.WriteLine("Aspect ratio set to: " + aspectRatio);
+                Console.WriteLine("Temp directory is: " + tempDirectory);
+                Console.WriteLine("Auto save time set to: " + autoSaveTime);
+                Console.WriteLine("Show status bar: " + showStatusBar);
+            }
+        }
+        public static void menu() {
+            do
+            {
+                Console.WriteLine($"Menu:\nDisplay data:\t1\nInput data:\t2\nSearch by license plate:\t3\nDelete record:\t4\nSort records:\t5\n");
+
+
+
+
+
+
+
+
+            }
+            while (true);
+
+
+        }
+
+        public static Car inputData()
+        { 
+            Car tempCar = new Car();
+			Console.WriteLine("Creating new Car...");
+
+			/*
+            private string licensePlate;
+            private string brand;
+            private string name;
+            private string color;
+            private double price;
+            private int horsePower;
+            private double[] consumption = new double[3];
+            private bool isEconomy;
+            */
+
+			Console.Write("Input the car's license plate: ");
+            tempCar.LicensePlate = getValidLicensePlate();
+
+			Console.Write("Input the car's brand: ");
+            tempCar.Brand = Console.ReadLine();
+
+			Console.Write("Input the car's name: ");
+            tempCar.Name = Console.ReadLine();
+
+			Console.Write("Input the car's color: ");
+            tempCar.Color = Console.ReadLine();
+
+			Console.Write("Input the car's price: ");
+            tempCar.Price = getValidDouble();
+
+			Console.Write("Input the car's horsepower: ");
+			tempCar.Price = getValidInt();
+
+			Console.Write("Inpupt the car's consumption (city, mixed, highway): ");
+            tempCar.SetConsumption(getValidDouble(), getValidDouble(), getValidDouble());
+
+            return tempCar;
+		}
+
+		public static int getValidInt()
+		{
+			int inputInt = 0;
+			if (!int.TryParse(Console.ReadLine(), out inputInt))
+			{
+				Console.WriteLine("Input is not a valid integer value.");
+				return getValidInt();
+			}
+			return inputInt;
+		}
+		public static double getValidDouble()
+        {
+            double inputDouble = 0;
+            if (!double.TryParse(Console.ReadLine(),out inputDouble))
+            {
+				Console.WriteLine("Input is not a valid double value.");
+                return getValidDouble();
+            }
+            return inputDouble;
+        }
+
+        public static string getValidLicensePlate()
+        {
+			string inputLicense = "";
+            inputLicense = Console.ReadLine();
+			if (!Car.IsValidLicensePlate(inputLicense))
+            {
+				Console.WriteLine("Valid formats: ABC-123 or AA-AA-123");
+                return getValidLicensePlate();
+			}
+            return inputLicense;
+        }
+
+        public static void SortOptions(int opt) {
+            //wip xddd
+            
+            return;
+        
+        }
+        static void Main(string[] args)
+        {
+            WriteToFile();
+            DisplayValues();
+            //Car testCar = new Car("IAP-512","Ford","Focus Mk1",75,"Gray",879000,7.6,6.3,5.5);
+            Car testCar = inputData();
+            Console.WriteLine(testCar);
+            Console.ReadKey();
+
+        }
+    }
+}
